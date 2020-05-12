bf4c7db9856c0a71393af12b4923cedcd57673ef
Samantha Copland <sam.copland1992@gmail.com>
2020-05-12 17:31:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ function multiply (a,b) {return (a*b)}
 function divide (a,b) {return (a/b)}
 function increment (n) {return (n += 1)}
 function decrement (n) {return (n -= 1)}
-function makeInt (n) {return parseInt ('n' , 10)}
-function preserveDecimal (n) {return parseFloat ("n")}
+function makeInt (n)
+ {return parseInt (n , 10)}
+function preserveDecimal (n) 
+{return parseFloat ("n")}
