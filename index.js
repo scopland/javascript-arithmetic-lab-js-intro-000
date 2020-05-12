44f64d4c6b5b5bf41316e13747c42545babc39e9
Samantha Copland <sam.copland1992@gmail.com>
2020-05-12 15:50:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ function subtract (a,b) {return (a-b)}
 function multiply (a,b) {return (a*b)}
 function divide (a,b) {return (a/b)}
 function increment (n) {return (n += 1)}
+function decrement (n) {return (n -= 1)}
